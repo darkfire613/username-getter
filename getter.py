f1dc220a56b85d0049c7c29c8ba6bc216f47309d
darkfire613 <darkfire613@me.com>
2015-01-16 17:44:35 -0800

    pulls usernames

--- getter.py
+++ getter.py
@@ -4,20 +4,28 @@
 # January 16, 2015
 
 import praw
+import time
 
-#initializes reddit api stuff
-user_agent = ("username skimmer 0.1 by /u/darkfire613 "
+# initializes reddit api stuff
+user_agent = ("username getter 0.1 by /u/darkfire613 "
               "https://github.com/darkfire613/username-getter")
 r = praw.Reddit(user_agent = user_agent)
 
-#load sublist
+# load sublist
 sublist = []
 with open('sublist') as file:
     for line in file:
-        sublist.append('/r/' + line)
+        sublist.append(line)
 file.close()
 
-#strip newlines
+# strip newlines
 sublist = [x.strip() for x in sublist]
 
-print sublist
+# main loop
+while true:
+    for subname in sublist:
+        subreddit = r.get_subreddit(subname)
+        for submission in subreddit.get_new(limit=10):
+            username = submission.author
+            print username
+    time.sleep(1800)
