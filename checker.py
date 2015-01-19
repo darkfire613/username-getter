0539c3bc4081e2206d8d0b637e781ce54a46bacc
darkfire613 <darkfire613@me.com>
2015-01-18 21:43:29 -0800

    started filter

--- checker.py
+++ checker.py
@@ -1,4 +1,16 @@
 # checker.py
 # Checks grabbed usernames according to rules
 # Owen Monsma darkfire613@icloud.com
-# January 15, 2015
+# January 18, 2015
+
+import enchant
+
+# load list
+userlist = []
+with open('userlist') as file:
+    for line in file:
+        userlist.append(line)
+file.close()
+
+# strip newlines from list
+userlist = [x.strip() for x in userlist]
