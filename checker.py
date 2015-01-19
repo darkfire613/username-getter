f39fa2a844ff8ea94dcd19f2bb24dca2764a633a
darkfire613 <darkfire613@me.com>
2015-01-19 00:13:51 -0800

    checker strips numbers

--- checker.py
+++ checker.py
@@ -5,6 +5,16 @@
 
 import enchant
 
+# functions
+
+def HasNumbers (inputstring):
+    return any (char.isdigit() for char in inputstring)
+
+def CheckIfWord (inputstring):
+    # TODO: check if string is a word
+    return False
+
+
 # load list
 userlist = []
 with open('userlist') as file:
@@ -14,3 +24,18 @@ file.close()
 
 # strip newlines from list
 userlist = [x.strip() for x in userlist]
+
+print '\nbefore:'
+print userlist
+
+# passedusers stores all the usernames that pass the check
+passedusers = []
+
+# main loop
+for user in userlist:
+    print 'checking: ' + user
+    if not HasNumbers(user):
+        passedusers.append(user)
+
+print '\nafter:'
+print passedusers
